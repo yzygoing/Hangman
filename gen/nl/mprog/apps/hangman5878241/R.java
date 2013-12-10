23b148e722070f228231b2c669c83d1d311e7995
yzygoing <yvette.ducaneaux@student.uva.nl>
2013-12-10 15:47:22 +0100

    design doc update

--- gen/nl/mprog/apps/hangman5878241/R.java
+++ gen/nl/mprog/apps/hangman5878241/R.java
@@ -17,25 +17,40 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int hangman0=0x7f020000;
+        public static final int hangman1=0x7f020001;
+        public static final int hangman2=0x7f020002;
+        public static final int hangman3=0x7f020003;
+        public static final int hangman4=0x7f020004;
+        public static final int hangman5=0x7f020005;
+        public static final int hangman6=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int button1=0x7f090003;
+        public static final int button2=0x7f090004;
+        public static final int editText1=0x7f090002;
+        public static final int imageView1=0x7f090001;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hangman0=0x7f060003;
+        public static final int hello_world=0x7f060004;
+        public static final int new_game=0x7f060002;
+        public static final int settings=0x7f060005;
     }
     public static final class style {
         /** 
@@ -59,10 +74,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int small=0x7f040000;
     }
 }
--- src/nl/mprog/apps/hangman5878241/MainActivity.java
+++ src/nl/mprog/apps/hangman5878241/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.content.Context;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -11,8 +16,32 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Log.e("onCreate", "warning");
+		
+		// for debug purposes
+		Log.d("onCreate", "test");
+		
+		// set buttons 'new game' and 'settings'
+		final Button newgame = (Button) findViewById(R.id.button1);
+		Log.d("init button newgame", "test");
+		
+		// TODO: final Button settings = (Button) findViewById(R.id.button2);
+		
+		// init new game
+		newgame.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+	            Log.d("init new game", "test");
+	            
+			}
+
+			// Load RandomWord from small.xml
+			
+			
+		});
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,5 +49,7 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	
 
 }
